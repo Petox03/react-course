cee56fb7adedfd2631d290b780e769182392cf34
Alberto Ramón Sosa Martínez <petox.somart@outlook.com>
2023-08-09 16:57:59 -0600

    estaodos compuestos en componente clase

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,10 +1,12 @@
 import React from "react";
+const SECURITY_CODE = "paradigma";
 
 class ClassState extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            value: "",
             error: false,
             loading: false,
         };
@@ -16,28 +18,38 @@ class ClassState extends React.Component {
             setTimeout(() => {
                 console.log("Validando clase gei");
 
-                this.setState({loading: false});
+                if (SECURITY_CODE === this.state.value) {
+                    this.setState({ error: false, loading: false });
+                } else {
+                    this.setState({ error: true, loading: false });
+                }
 
-                console.log("Validación de clase terminada, gei");
             }, 3000);
         }
     }
 
     render() {
+
         return (
             <div>
                 <h2>Eliminar {this.props.name}</h2>
 
                 <p>Por favor, escribe el código de seguridad</p>
 
-                {this.state.error && (
+                {(this.state.error && !this.state.loading) && (
                     <p>Error: el código es incorrecto</p>
                 )}
                 {this.state.loading && (
                     <p>Cagando...</p>
                 )}
 
-                <input placeholder="Código de Seguridad" />
+                <input
+                    placeholder="Código de Seguridad"
+                    value={this.state.value}
+                    onChange={(event) => {
+                        this.setState({ value: event.target.value });
+                    }}
+                />
                 <button
                     onClick={() => { this.setState({ loading: true }) }}
                 >Comprobar</button>
